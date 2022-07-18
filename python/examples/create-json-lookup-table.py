0df8139804eb2f38a6b5b0155b7996ea6474f31a
Michael Halle <m@halle.us>
2022-07-18 12:14:22 -0400

    rename allen-sample to ccf-2017

--- python/examples/create-json-lookup-table.py
+++ python/examples/create-json-lookup-table.py
@@ -10,7 +10,7 @@ topdir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
 from openanatomy import parse_atlas
 
-SampleAtlas = urljoin(topdir, "atlases/allen-sample/atlas.oa.json")
+SampleAtlas = urljoin(topdir, "atlases/ccf-2017/atlas.oa.json")
 Context = urljoin(topdir, "contexts/openanatomy-context.jsonld")
 
 if __name__ == "__main__":
